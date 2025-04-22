84f1f5ea43fa7fcfd3fd3ce536ab121171fe00cd
Nate Del Duca <nate@nated.ca>
2025-04-21 23:54:27 -0400

    adjusting middleware logging

--- internal/app/app_api_middleware.go
+++ internal/app/app_api_middleware.go
@@ -7,8 +7,13 @@ import (
 
 func (a *App) LoggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		a.Log.Info("new request:", "method", r.Method, "path", r.URL.Path, "host", r.Host)
-		a.Log.Info(r.Header.Get("X-Forwarded-Host"))
+		if r.Host != "" {
+			a.Log.Info("new request:", "method", r.Method, "path", r.URL.Path, "host", r.Host)
+		} else if r.Header.Get("X-Forwarded-Host") != "" {
+			a.Log.Info("new request:", "method", r.Method, "path", r.URL.Path, "host", r.Header.Get("X-Forwarded-Host"))
+		} else {
+			a.Response(w, a.Err("host header and x-forwarded-host header not provided"), http.StatusNoContent)
+		}
 		next.ServeHTTP(w, r)
 	})
 }
